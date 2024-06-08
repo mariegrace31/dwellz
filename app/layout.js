8a5051900ca5deaae68024df48b126037936fa4d
mariegrace <mariegracebmg@gmail.com>
2024-06-08 23:30:52 +0200

    Update the layout

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
@@ -21,6 +22,6 @@ export default function RootLayout({ children }) {
         </body>
      </html>
     </ClerkProvider>
-    
+
   );
 }
