9eb298a552913350e1a0bc7d489aba20361cde60
mariegrace <mariegracebmg@gmail.com>
2024-06-25 11:59:32 +0200

    Install the react google places autocomplete package

--- app/(routes)/add-new-listing/page.jsx
+++ app/(routes)/add-new-listing/page.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 function AddNewListing() {
   return (
     <div className='p-10 flex flex-col gap-5 items-center justify-center'>
       <h2 className='font-bold text-2xl'>Add New Listing</h2>
       <div>
-        <h2>Enter the address you want to list</h2>
+        <h2 className='text-gray-500'>Enter the address you want to list</h2>
+        
+        <GooglePlacesAutocomplete
+      apiKey="****"
+    />
       </div>
     </div>
   )
