4d7017be2c24a7f622e24fea10296d2d990ec17e
mariegrace <mariegracebmg@gmail.com>
2024-06-27 10:26:01 +0200

    Style the places search area

--- app/(routes)/add-new-listing/page.jsx
+++ app/(routes)/add-new-listing/page.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import GoogleAddressSearch from '@/app/_components/GoogleAddressSearch';
+import { Button } from '@/components/ui/button';
 
 function AddNewListing() {
   return (
-    <div className='p-10 flex flex-col gap-5 items-center justify-center'>
-      <h2 className='font-bold text-2xl'>Add New Listing</h2>
-      <div>
-        <h2 className='text-gray-500'>Enter the address you want to list</h2>
-        <GoogleAddressSearch />
+    <div className='mt-10 md:mx-56 lg:mx-80'>
+      <div className='p-4 flex flex-col gap-3 items-center justify-center'>
+        <h2 className='font-bold text-2xl'>Add New Listing</h2>
+        <div className='p-5 w-full rounded-lg border shadow-md flex flex-col gap-4 justify-center items-center'>
+          <h2 className='text-gray-500'>Enter the address you want to list</h2>
+          <GoogleAddressSearch />
+          <Button>Next</Button>
+        </div>
       </div>
     </div>
   )
--- app/_components/GoogleAddressSearch.jsx
+++ app/_components/GoogleAddressSearch.jsx
@@ -1,12 +1,22 @@
 "use client"
 import React from 'react';
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import { FaMapMarkerAlt } from "react-icons/fa";
 
 function GoogleAddressSearch() {
   return (
-    <div>
+    <div className='flex gap-1 items-center w-full pt-3'>
+      <FaMapMarkerAlt className='h-10 w-12 text-primary rounded-lg bg-green-200 p-2' />
       <GooglePlacesAutocomplete
         apiKey={process.env.NEXT_PUBLIC_GOOGLE_PLACE_API_KEY}
+        selectProps={{
+          placeholder:'Search Property Address',
+          isClearable:true,
+          className:'w-full',
+          onChange:(place)=>{
+            console.log(place);
+          }
+        }}
       />
     </div>
   )
